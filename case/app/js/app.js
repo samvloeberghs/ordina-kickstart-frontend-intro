1ba26c9c4474201e884bd87be599db1e03c9450a
Sam Vloeberghs <samvloeberghs@Sams-MacBook-Pro.local>
2015-08-19 16:48:28 +0200

    updated solved case

--- case/app/js/app.js
+++ case/app/js/app.js
@@ -1,24 +1,79 @@
 'use strict';
 
-var app = (function(document, $) {
+var app = (function (document, $, window) {
 
-	var docElem = document.documentElement,
-		_userAgentInit = function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		},
-		_init = function() {
-			$(document).foundation();
-			_userAgentInit();
-		};
+    var docElem = document.documentElement;
 
-	return {
-		init: _init
-	};
+    var _userAgentInit = function () {
+            docElem.setAttribute('data-useragent', navigator.userAgent);
+        },
+        _mobileNavInit = function () {
+            $(document).on('change', 'header select', function (e) {
+                e.preventDefault();
+                window.location = $(this).val();
+                return false;
+            });
+        },
+        _bindContactFormValidator = function(){
+            initFormValidator($('#contact form'));
+        };
 
-})(document, jQuery);
+    var init = function () {
+        $(document).foundation();
+        _userAgentInit();
+        _mobileNavInit();
+        _bindContactFormValidator();
+    };
 
-(function() {
+    return {
+        init: init
+    };
 
-	app.init();
+})(document, jQuery, window);
+
+(function () {
+
+    app.init();
+
 
 })();
+
+
+function initFormValidator(form){
+
+    $(document).on('submit', form, function(e){
+
+        e.preventDefault();
+
+        var valid = true;
+        $('.errormsg', form).remove();
+        $('.error', form).removeClass('error');
+
+        $('input, select, textarea').each(function(){
+
+            var $this = $(this);
+            var $parent = $(this).parent();
+
+            var isRequired = $this.hasClass('required');
+            var isEmail = $this.hasClass('email');
+
+            if(isRequired && ! $(this).val().length){
+                $this.addClass('error');
+                $parent.append('<p class="errormsg">Veld is verplicht!</p>');
+                valid = false;
+            }
+
+            var regexp = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+            if(isEmail && !regexp.test($(this).val())){
+                $this.addClass('error');
+                $parent.append('<p class="errormsg">Ongeldig emailadres!</p>');
+            }
+
+        });
+
+        return valid;
+
+    });
+
+}
+
